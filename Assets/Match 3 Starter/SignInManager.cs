76dfccb8f74932d77c28032f45aa6e6688566e07
GrafRoy <89658517+GrafRoy@users.noreply.github.com>
2021-11-23 09:23:20 -0600

    Add files via upload

--- Assets/Match 3 Starter/SignInManager.cs
+++ Assets/Match 3 Starter/SignInManager.cs
@@ -6,6 +6,7 @@ using Firebase;
 using Firebase.Auth;
 using UnityEngine.SceneManagement;
 using Firebase.Database;
+using System.Diagnostics;
 
 public class SignInManager : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class SignInManager : MonoBehaviour
     public FirebaseAuth auth;
     public Firebase.Auth.FirebaseUser newUser; 
     public DatabaseReference DBreference;
-    
+    public FirebaseDatabase database = FirebaseDatabase.GetInstance("https://match3tiles-cf880-default-rtdb.firebaseio.com/");
 
     // Start is called before the first frame update
 
@@ -38,59 +39,65 @@ public class SignInManager : MonoBehaviour
     {
         auth.CreateUserWithEmailAndPasswordAsync(email = UsernameInputField.text, password = PasswordInputField.text).ContinueWith(task => {
             if (task.IsCanceled) {
-            Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
             return;
             }
             else if (task.IsFaulted) {
-            Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
             return;
-            }
-            else
-            {
-                newUser = task.Result;
-            }
-
-            // Firebase user has been created.
-            string json = JsonUtility.ToJson(newUser);
-            var mDatabaseRef = DBreference.Child("users").Child(email).SetRawJsonValueAsync(json);
-            Debug.LogFormat("This is working", newUser.DisplayName, newUser.UserId);
-    
-            
-            
+            }    
         });
+
+
+        DatabaseNewWrite();
+        PopUpWindow();
+
     }
 
-    public void OnSignInButtonClicked() => auth.SignInWithEmailAndPasswordAsync(email = UsernameInputField.text, password = PasswordInputField.text).ContinueWith(task =>
-    {
-        if (task.IsCanceled)
+    public void OnSignInButtonClicked()
+    { 
+        auth.SignInWithEmailAndPasswordAsync(email = UsernameInputField.text, password = PasswordInputField.text).ContinueWith(task =>
         {
-            Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+            if (task.IsCanceled)
+            {
+                return;
+            }
+            else if (task.IsFaulted)
+            {
             return;
-        }
-        else if (task.IsFaulted)
-        {
-            Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
-            return;
-        }
-        else
-        {
-            newUser = task.Result;
-            Panel.SetActive(true);
-        }
-        newUser = task.Result;
+            } 
 
     });
+
+        //DatabaseWrite();
+        PopUpWindow();
+    }
+
+    public void DatabaseNewWrite()
+    {
+        //string json = JsonUtility.ToJson(email);
+        //DBreference.Child("User").Child(UsernameInputField.text).SetRawJsonValueAsync(json).ContinueWith(task => {
+        //if (task.IsCanceled)
+        //{
+        //  UnityEngine.Debug.Log("successfully added data to firebase");
+        //}
+        //else
+        //{
+        //  UnityEngine.Debug.Log("not successful");
+        //}
+        //});
+        DBreference.Push().Child("User").Child(email).SetValueAsync(email);
+    }
+
 
     public void PopUpWindow()
     {
         if(Panel.activeSelf == false)
         {
-            Debug.Log("Shit ain't workin");
+        
             Panel.SetActive(true);
         }
         else if(Panel.activeSelf == true)
         {
-            Debug.Log("Why this shit up");
+            
             Panel.SetActive(false);
         }
     }
